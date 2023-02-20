a88271da4abaafeff59bb7ab00c97593296a4960
Taiki-nk <taikifate.0206@gmail.com>
2023-02-21 00:07:01 +0900

    save commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+	  <>
+		  <p>header</p>
+		  <button>A</button>
+		  <button>B</button>
+	  </>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
+import { createContext, useContext } from "react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+
+const ParamContext=createContext({apiName:"user"});
 
 const Home: NextPage = () => {
-  return <></>;
+  const param = useContext(ParamContext);
+
+  return (
+    <ParamContext.Provider value={param}>
+      <Header />
+      <MainContent />
+    </ParamContext.Provider>
+  );
+};
+
+const MainContent = () => {
+  const param = useContext(ParamContext);
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>{param.apiName}</p>
+    </>
+  );
 };
 
 export default Home;
